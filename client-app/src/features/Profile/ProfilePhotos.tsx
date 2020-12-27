e322ce3f8baade2d8d9cb11346b682ec362e7a55
JAMES HERRERO <100859697@student.swin.edu.au>
2020-12-27 19:34:21 +1100

    Add skeleton of add photo button to photo profile

--- client-app/src/features/Profile/ProfilePhotos.tsx
+++ client-app/src/features/Profile/ProfilePhotos.tsx
@@ -1,22 +1,52 @@
 import { observer } from 'mobx-react-lite';
-import React, { useContext } from 'react'
-import { Card, Header, Tab, Image } from 'semantic-ui-react'
+import React, { useContext, useState } from 'react'
+import { Card, Header, Tab, Image, Button, Grid } from 'semantic-ui-react'
 import { RootStoreContext } from '../../app/stores/rootStore';
 
 function ProfilePhotos() {
   const rootStore = useContext(RootStoreContext);
-  const { profile } = rootStore.profileStore;
+  const { profile, isCurrentUser } = rootStore.profileStore;
+
+  // trigger toggle for add photo mode..
+  const [addPhotoMode, setAddPhotoMode] = useState(false);
+
   return (
     <Tab.Pane>
-      <Header icon="image" content='Photos' />
-      <Card.Group itemsPerRow={5}>
-        {profile && profile?.photo.map(p => (
-            <Card key={p.id}>
-              <Image src={p.url}/>
-            </Card>
-          )
-        )}
-      </Card.Group>
+      <Grid>
+        <Grid.Column width={16} style={{paddingBottom: '0'}}>
+        <Header floated='left' icon="image" content='Photos' />
+          { isCurrentUser &&
+            <Button
+              floated='right'
+              basic
+              content={addPhotoMode ? 'Cancel' : 'Add Photo'}
+              onClick={() => setAddPhotoMode(!addPhotoMode)}/>
+          }
+        </Grid.Column>
+        <Grid.Column width={16}>
+          { addPhotoMode ? (
+            <p>Photo widget will go here</p>
+          ): (
+            <Card.Group itemsPerRow={5}>
+              {profile && profile?.photo.map(p => (
+                  <Card key={p.id}>
+                    <Image src={p.url}/>
+                    { isCurrentUser &&
+                      <Button.Group fluid widths={2}>
+                        <Button basic positive content='main'/>
+                        <Button basic negative icon='trash'/>
+                      </Button.Group>
+                    }
+                  </Card>
+                )
+              )}
+            </Card.Group>
+          )}
+
+        </Grid.Column>
+      </Grid>
+
+
     </Tab.Pane>
   )
 }
