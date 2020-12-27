425018f4f64b490c8604b640066a7de0d6bdc3f0
JAMES HERRERO <100859697@student.swin.edu.au>
2020-12-27 19:41:26 +1100

    set profile page to default go to photos so it

--- client-app/src/features/Profile/ProfileContent.tsx
+++ client-app/src/features/Profile/ProfileContent.tsx
@@ -4,8 +4,8 @@ import ProfilePhotos from './ProfilePhotos';
 
 const panes = [
   {menuItem: 'About', render: () => <Tab.Pane>About content</Tab.Pane>},
-  {menuItem: 'Photos', render: () => <ProfilePhotos/>},
   {menuItem: 'Activities', render: () => <Tab.Pane>Activities content</Tab.Pane>},
+  {menuItem: 'Photos', render: () => <ProfilePhotos/>},
   {menuItem: 'Followers', render: () => <Tab.Pane>Followers content</Tab.Pane>},
   {menuItem: 'Following', render: () => <Tab.Pane>Following content</Tab.Pane>},
 ]
@@ -16,6 +16,7 @@ const ProfileContent = () => {
       menu={{fluid: true, vertical: true}}
       menuPosition='right'
       panes={panes}
+      activeIndex={2}
     />
   )
 }
