565db2b778fab86c3e91ffd2d6df02c5edcef743
JAMES HERRERO <100859697@student.swin.edu.au>
2021-01-31 13:05:53 +1100

    Add Cors policy to accept allow with credentials to accept tokens via signalr method of communication

--- API/Startup.cs
+++ API/Startup.cs
@@ -121,7 +121,7 @@ namespace API
       {
         options.AddPolicy("CorsPolicy", policy =>
         {
-          policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+          policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000").AllowCredentials();
         });
       });
 
