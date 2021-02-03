828556a435f6ee47b421b445e7f66505de80e728
JAMES HERRERO <100859697@student.swin.edu.au>
2021-02-03 17:41:18 +1100

    Add fix for creating activities and date time format for comments

--- client-app/src/features/Activities/Details/ActivityDetailChat.tsx
+++ client-app/src/features/Activities/Details/ActivityDetailChat.tsx
@@ -5,6 +5,7 @@ import { Form as FinalForm, Field } from "react-final-form";
 import { Link } from "react-router-dom";
 import TextAreaInput from "../../../app/common/form/TextAreaInput";
 import { observer } from "mobx-react-lite";
+import { formatDistance } from "date-fns";
 
 const ActivityDetailChat = () => {
   const rootStore = useContext(RootStoreContext);
@@ -38,7 +39,7 @@ const ActivityDetailChat = () => {
                 <Comment.Content>
                   <Comment.Author as={Link} to={`/profile/${comment.username}`}>{comment.displayName}</Comment.Author>
                   <Comment.Metadata>
-                    <div>{comment.createdAt}</div>
+                    <div>{formatDistance(comment.createdAt, new Date())}</div>
                   </Comment.Metadata>
                   <Comment.Text>{comment.body}</Comment.Text>
                 </Comment.Content>
